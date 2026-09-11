30076c9493dcc5001fc649a98838847aef1ed88b
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-09-11 20:19:19 +0600

    update header sticky fetatures

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
 
       <NavHeader></NavHeader>
       <Hero></Hero>
+      <Hero></Hero>
 
 
     </>
--- src/Components/NavHeader.tsx
+++ src/Components/NavHeader.tsx
@@ -3,7 +3,7 @@ const NavHeader = () => {
     return (
         <>
 
-            <nav className='container mx-auto px-7'>
+            <nav className='container sticky z-50 top-0 bg-white mx-auto px-7'>
                 <div className='flex justify-between items-center py-5 border-b-2 border-slate-200'>
                     <img className='h-12' src={Logo} alt="DevStack Logo" />
                     <ul className='flex gap-7 text-[#475569] items-center justify-center'>
