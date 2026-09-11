cb0d5d98dc3ed1d1e1b9fc3c388d7e790a4bb4f6
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-09-11 17:22:42 +0600

    Hero section style update

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -2,11 +2,12 @@ import HeroImage from '../assets/banner-stack.png'
 
 const Hero = () => {
     return (
-        <div className="container mx-auto flex justify-between items-center">
+
+        <div className=" max-w-7xl mx-auto flex justify-between items-center">
             <div className='w-1/2 space-y-2'>
                 <h2 className='text-6xl font-extrabold '>Build Your Ideal <br /> <span className='bg-gradient-to-r from-pink-500 to-purple-600 bg-clip-text text-transparent'>Development Stack</span></h2>
 
-                <p className='mb-6'>
+                <p className='mb-6 text-sm text-[#475569]'>
                     Explore frontend, backend, database and tooling options, compare them side by side, and put together the stack that fits your next project.
                 </p>
                 <div className='flex gap-4'>
@@ -17,8 +18,9 @@ const Hero = () => {
             <div>
                 <img src={HeroImage} alt="" />
             </div>
-
         </div>
+
+
     );
 };
 
