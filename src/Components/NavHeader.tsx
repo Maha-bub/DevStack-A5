e84aa693a2230a32bd658e291ece03068331cfee
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-09-11 16:30:01 +0600

    update nav styles

--- src/Components/NavHeader.tsx
+++ src/Components/NavHeader.tsx
@@ -2,24 +2,24 @@ import Logo from '../assets/logo-text.png'
 const NavHeader = () => {
     return (
         <>
-            <header >
-                <nav className='container mx-auto px-4'>
-                    <div className='flex justify-between items-center h-20'>
-                        <img className='h-14' src={Logo} alt="DevStack Logo" />
-                        <ul className='flex gap-7 items-center justify-center'>
-                            <li><a href="">Home</a></li>
-                            <li><a href="">Technologies</a></li>
-                            <li><a href="">Projects</a></li>
-                            <li><a href="">About</a></li>
-                            <li><a href="">Contact</a></li>
-                        </ul>
-                        <div className='flex gap-4'>
-                            <button className="rounded-full text-sm font-semibold py-2.5 px-5">Sign In</button>
-                            <button className="rounded-full text-sm font-semibold py-2.5 px-5 text-white bg-[#D91B7E]">Sign Up</button>
-                        </div>
+
+            <nav className='container mx-auto px-7'>
+                <div className='flex justify-between items-center py-5 border-b-2 border-slate-200'>
+                    <img className='h-12' src={Logo} alt="DevStack Logo" />
+                    <ul className='flex gap-7 text-[#475569] items-center justify-center'>
+                        <li><a className='text-[#D91B7E] ' href="">Home</a></li>
+                        <li><a href="">Technologies</a></li>
+                        <li><a href="">Projects</a></li>
+                        <li><a href="">About</a></li>
+                        <li><a href="">Contact</a></li>
+                    </ul>
+                    <div className='flex gap-4'>
+                        <button className="rounded-full text-sm font-semibold py-2.5 px-5">Sign In</button>
+                        <button className="rounded-full text-sm font-semibold py-2.5 px-5 text-white bg-[#D91B7E]">Sign Up</button>
                     </div>
-                </nav>
-            </header>
+                </div>
+            </nav>
+
 
         </>
     );
